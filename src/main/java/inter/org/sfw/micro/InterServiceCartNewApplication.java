27e4284e6df36ffae78f122bd5f53590d9708482
ShankarTogari <ShankarThogarii@gmail.com>
2023-01-20 08:16:55 +0530

    STA001-New Cart Service

---
+++ src/main/java/inter/org/sfw/micro/InterServiceCartNewApplication.java
@@ -0,0 +1,13 @@
+package inter.org.sfw.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterServiceCartNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterServiceCartNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inter/org/sfw/micro/InterServiceCartNewApplicationTests.java
@@ -0,0 +1,13 @@
+package inter.org.sfw.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterServiceCartNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
